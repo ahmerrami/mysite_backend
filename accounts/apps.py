7e23a3640448dbc9f7828ae5473d653c241056ad
ahmederrami <ahmederrami@gmail.com>
2025-07-30 12:31:18 +0000

    ne pas versionner les fichiers fixtures

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,9 +1,14 @@
+# /accounts/apps.py
 from django.apps import AppConfig
-
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals
+        import sys
+
+        # pas de signaux lorsqu'il s'agit de loaddata
+        if 'loaddata' in sys.argv or 'migrate' in sys.argv:
+            return
+        from . import signals
