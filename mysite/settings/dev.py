5e61b0aa8d3837802432de0299f1d10c0d880349
webmaster <webmaster@srv-one.pve.local>
2025-08-29 18:04:35 +0200

    diverses modifs

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -5,7 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['supratourstravel.pythonanywhere.com']
+ALLOWED_HOSTS = ['supratourstravel.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Configuration SQLite (sans dépendance externe)
 DATABASES = {
