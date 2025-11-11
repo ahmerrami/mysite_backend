445e0100a4d315bfea53332936e8eea54aef4de3
ahmerrami <ahmederrami@gmail.com>
2025-11-11 21:06:32 +0100

    correction gestion fichiers statics

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -18,7 +18,8 @@ from decouple import config
 SOCIETE = config('SOCIETE')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# settings/base.py -> settings/ -> mysite/ -> mysite_backend/ (racine)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
