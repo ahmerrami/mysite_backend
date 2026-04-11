3386dfbb518a1f09b129ffd23b68d384ce653fae
errami <ahmederrami@gmail.com>
2026-04-11 14:20:31 +0100

    correction des imports dans fournisseurs.models.py

--- fournisseurs/models.py
+++ fournisseurs/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 from django.conf import settings
 from django.core.validators import RegexValidator, FileExtensionValidator, MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
